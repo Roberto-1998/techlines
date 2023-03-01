9484ff713aca97976564a9ba523de37f2cad63e0
Roberto <rcastellong98@gmail.com>
2023-03-01 21:51:44 +0100

    [add] redux setup and products etched from API

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -18,7 +18,7 @@ import { AiFillStar } from 'react-icons/ai';
 import { Link as ReactLink } from 'react-router-dom';
 import { useState } from 'react';
 
-const Rating = ({ rating, numReviews }) => {
+const Rating = ({ rating, numberOfReviews }) => {
   const { iconSize } = useState('14px');
 
   return (
@@ -31,7 +31,7 @@ const Rating = ({ rating, numReviews }) => {
         <AiFillStar size={iconSize} w={'14px'} color={rating >= 5 ? '#E19E73' : '#EBEFF3'} />
       </HStack>
       <Text fontSize={'md'} fontWeight={'bold'} ml={'4px'}>
-        {`${numReviews} ${numReviews === 1 ? 'Review' : 'Reviews'}`}
+        {`${numberOfReviews} ${numberOfReviews === 1 ? 'Review' : 'Reviews'}`}
       </Text>
     </Flex>
   );
@@ -50,7 +50,7 @@ const ProductCard = ({ product }) => {
       shadow={'lg'}
       position={'relative'}
     >
-      {product.isNew && <Circle size={'10px'} position={'absolute'} top={2} right={2} bg={'green.300'} />}
+      {product.productIsNew && <Circle size={'10px'} position={'absolute'} top={2} right={2} bg={'green.300'} />}
       {product.stock <= 0 && <Circle size={'10px'} position={'absolute'} top={2} right={2} bg={'red.300'} />}
       <Image src={product.image} alt={product.name} roundedTop={'lg'} />
       <Box flex={'1'} maxH={'5'} alignItems={'baseline'}>
@@ -59,7 +59,7 @@ const ProductCard = ({ product }) => {
             Sold out
           </Badge>
         )}
-        {product.isNew && (
+        {product.productIsNew && (
           <Badge rounded={'full'} px={'2'} fontSize={'0.8em'} colorScheme='green'>
             New
           </Badge>
@@ -73,7 +73,7 @@ const ProductCard = ({ product }) => {
         </Link>
       </Flex>
       <Flex justifyContent={'space-between'} alignContent={'center'} py={'2'}>
-        <Rating rating={product.rating} numReviews={product.numReviews} />
+        <Rating rating={product.rating} numberOfReviews={product.numberOfReviews} />
       </Flex>
       <Flex justify={'space-between'}>
         <Box fontSize={'2x1'} color={useColorModeValue('gray.800', 'white')}>
